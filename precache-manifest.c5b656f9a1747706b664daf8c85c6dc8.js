dc582179894a52f5f524244e816593fd04e95f66
Yulin He <YHe@BroadViewSoftware.com>
2019-07-31 11:19:16 -0400

    Updates

---
+++ precache-manifest.c5b656f9a1747706b664daf8c85c6dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d40ad476b3f4d2a92f99ccaf207fa2",
+    "url": "/survey-to-chart/index.html"
+  },
+  {
+    "revision": "7da9b9fe4dd8359354a7",
+    "url": "/survey-to-chart/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "28b4b28ae3f36be7346e",
+    "url": "/survey-to-chart/static/css/main.d9d204ee.chunk.css"
+  },
+  {
+    "revision": "7da9b9fe4dd8359354a7",
+    "url": "/survey-to-chart/static/js/2.280639c5.chunk.js"
+  },
+  {
+    "revision": "28b4b28ae3f36be7346e",
+    "url": "/survey-to-chart/static/js/main.72dad342.chunk.js"
+  },
+  {
+    "revision": "ae9b7e91314acedddcdf",
+    "url": "/survey-to-chart/static/js/runtime~main.d5e87a64.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/survey-to-chart/precache-manifest.f99c088b0fc60733cdf770827b7bdef5.js"
+  "/survey-to-chart/precache-manifest.c5b656f9a1747706b664daf8c85c6dc8.js"
 );
 
 self.addEventListener('message', (event) => {
